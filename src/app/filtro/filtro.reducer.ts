3b0c4bbb52f8645972068959d87c7550cfc22f4c
josealvarorarenas <80318453+josealvarorarenas@users.noreply.github.com>
2021-11-16 17:50:21 +0100

    Primer commit

---
+++ src/app/filtro/filtro.reducer.ts
@@ -0,0 +1,15 @@
+import { createReducer, on, Action } from '@ngrx/store';
+import { setFiltro, filtrosValidos } from './filtro.actions';
+
+
+
+export const initialState: filtrosValidos = 'todos';
+
+const _filtroReducer = createReducer<filtrosValidos, Action>(initialState,
+  on( setFiltro, (state, { filtro }) => filtro ),
+
+);
+
+export function filtroReducer( state, action ){
+  return _filtroReducer(state, action);
+}
